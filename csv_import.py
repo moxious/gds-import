fbdec77945af93dcdfdd1cb069df1b582797c327
Moxious <moxious@oldhat.org>
2020-11-23 10:23:16 -0500

    iterate

--- csv_import.py
+++ csv_import.py
@@ -16,7 +16,9 @@ import sys
 import subprocess
 import os
 
-import_args = ["neo4j-admin", "import"]
+# Place where google storage files are copied to
+STAGING_DIR = "/tmp/staging"
+neo4j_path = os.environ.get('NEO4J_HOME', '/var/lib/neo4j')
 
 def csvFilesInDirectory(dir):    
     files = listdir(dir)
@@ -55,37 +57,67 @@ def log(msg):
     print("%s csv_import: %s" % (datetime.now().isoformat(), msg))
 
 def copy_data_from_storage(import_from):
-    staging = "staging"
-    if not os.path.exists(staging):
-        mkdir(staging)
-    result = subprocess.run([
-        "gsutil", "-m", "rsync", "-r", import_from, staging
-    ], stderr=subprocess.STDOUT)
+    if not os.path.exists(STAGING_DIR):
+        mkdir(STAGING_DIR)
 
+    result = run_command("gsutil -m cp -r \"%s\" \"%s\"" % (import_from, STAGING_DIR))
     log("Finished copying with %s" % result)
-    log("Files available are %s" % listdir(staging))
-    return staging
+
+    # Suppose we've copied gs://mybucket/mydataset to the staging dir
+    # the available file list is now just "mydataset", or should be.
+    available = listdir(STAGING_DIR)
+    log("Files available are %s" % listdir(STAGING_DIR))
+
+    if len(available) == 1:
+        return join(STAGING_DIR, available[0])
+
+    raise Exception("Unexpected directory contents in %s after copy; did you specify the dataset location correctly?" % STAGING_DIR)
 
 def catcher(fn, message):
     try:
-        return fn()
+        result = fn()
+        log("%s: %s" % (message, result))
+        return result
     except Exception as err:
         log("Failed to %s: %s" % (message, err))
 
+def run_command(cmd):
+    log("COMMAND: %s" % cmd)
+    return subprocess.run(cmd, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+
 def start_neo4j():
-    return catcher(lambda: subprocess.run(['systemctl', 'start', 'neo4j']), "start neo4j system service")
+    log("Starting Neo4j system service")
+    return catcher(lambda: run_command("systemctl start neo4j"), "start neo4j system service")
 
 def stop_neo4j():
-    return catcher(lambda: subprocess.run(['systemctl', 'stop', 'neo4j']), "stop neo4j system service")
+    log("Stopping Neo4j system service")
+    return catcher(lambda: run_command('systemctl stop neo4j'), "stop neo4j system service")
+
+def destroy_database(db='neo4j'):
+    log("Destroying in place 'neo4j' database, to be replaced by import")
+
+    commands = [
+        { "cmd": "rm -rf \"%s/data/databases/%s\"" % (neo4j_path, db), "message": "destroy database files" },
+        { "cmd": "rm -rf \"%s/data/transactions/%s\"" % (neo4j_path, db), "message": "destroy tx logs" },
+        { "cmd": "rm -f \"%s/data/databases/store_lock\"" % neo4j_path, "message": "destroy store_lock" }
+    ]
+    
+    for command in commands: 
+        catcher(lambda: run_command(command['cmd']), command['message'])
+
+    return True
 
 def assign_permissions():
+    log("Assigning neo4j:neo4j permissions to newly created database")
     return catcher(
-        lambda: subprocess.run(["chown", "-R", "neo4j:neo4j", "/var/lib/neo4j/data/*"], shell=True), 
+        lambda: run_command("chown -R neo4j:neo4j /var/lib/neo4j/data/*"), 
         "assign neo4j:neo4j privileges to restored database")
 
-def main():
+def main(storage = "gs://meetup-data/chicago_crime_bigquery"):
+    """Takes an argument of where the CSV import set is located (a directory)"""    
     stop_neo4j()
-    dir = copy_data_from_storage("gs://meetup-data/chicago_crime_bigquery")
+    destroy_database('neo4j')
+    dir = copy_data_from_storage(storage)
 
     data_dirs = [f for f in listdir(dir) if isdir(join(dir, f))]
     log(data_dirs)
@@ -95,25 +127,34 @@ def main():
     log("Nodes: %s" % nodes)
     log("Relationships: %s" % rels)
 
+    if len(nodes) == 0:
+        raise Exception("No valid node CSV directories could be found; there is nothing to import!")
+
+    # Iteratively build the command that will be executed to do import
+    import_args = ["neo4j-admin", "import"]
+
     for node_dir in nodes:
-        label=node_dir.replace("node_", "")
+        label = node_dir.replace("node_", "")
         import_args.append(prepare_node(label, join(dir, node_dir)))
 
     for rel_dir in rels:
-        rel_type=rel_dir.replace("rel_", "")
+        rel_type = rel_dir.replace("rel_", "")
         import_args.append(prepare_rel(rel_type, join(dir, rel_dir)))
 
     command=" ".join(import_args)
     log("Running command %s" % command)
     result = subprocess.run(import_args, stderr=subprocess.STDOUT)
-    print("Finished neo4j-admin import")
+    log("Finished neo4j-admin import; next assigning permissions")
     assign_permissions()
+    log("Re-starting neo4j system service with the new database")
     start_neo4j()
 
-try:
-    main()
-    sys.exit(0)
-except Exception as err:
-    log("Unhandled exception in import process")
-    log("%s" % err)
-    sys.exit(1)
+if __name__ == "__main__":
+    # execute only if run as a script
+    try:
+        main()
+        sys.exit(0)
+    except Exception as err:
+        log("Unhandled exception in import process")
+        log("%s" % err)
+        sys.exit(1)
--- launch-instance.sh
+++ launch-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Choose your settings
-NAME=my-graph
+NAME=my-graph-$(head -c 25 /dev/urandom | md5 | head -c 4)
 MACHINE_TYPE=e2-highmem-8
 
 # The purpose of this script it to launch a stand-alone GCP VM that 
@@ -16,6 +16,7 @@ gcloud compute firewall-rules create allow-neo4j-bolt-https \
 
 # The results of the import script will be written to /var/log/syslog inside of the
 # VM
+echo "Creating virtual machine $NAME ..."
 gcloud compute instances create $NAME \
     --image neo4j-enterprise-1-4-1-3-apoc \
     --machine-type=$MACHINE_TYPE \
